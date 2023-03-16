8a8f274a5486f7b65f791f9a1a77422fb6e8ebec
luis-designs <luislozano120xd@gmail.com>
2023-03-16 11:52:55 -0500

    :sparkles: Levantar Feedback de Jodry

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,5 +2,40 @@
 import * as bootstrap from 'bootstrap';
 
 import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
+// import 'aos/dist/aos.css'; // You can also use <link> for styles
+// let aosStyles = document.getElementById('aosStylesheet');
 AOS.init();
+
+const btnActiveModal = document.querySelectorAll(
+  'button[data-bs-toggle="modal"]'
+);
+
+let aosStylesheet = document.getElementById('aosStylesheet');
+
+const createStylesheetAOS = () => {
+  aosStylesheet = document.createElement('link');
+  aosStylesheet.rel = 'stylesheet';
+  aosStylesheet.href = 'https://unpkg.com/aos@2.3.1/dist/aos.css';
+  aosStylesheet.id = 'aosStylesheet';
+  document.head.appendChild(aosStylesheet);
+};
+
+const removeStylesheetAOS = () => {
+  if (aosStylesheet) {
+    aosStylesheet.remove();
+    aosStylesheet = null;
+  }
+};
+
+const clickOut = (element) => {
+  element.addEventListener('click', (e) => {
+    createStylesheetAOS();
+  });
+};
+
+btnActiveModal.forEach((btnModal) =>
+  btnModal.addEventListener('click', () => {
+    removeStylesheetAOS();
+    clickOut(btnModal.nextElementSibling.firstElementChild.firstElementChild);
+  })
+);
